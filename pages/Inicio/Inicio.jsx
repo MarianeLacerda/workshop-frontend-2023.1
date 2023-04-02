47e73baf562aab47537297dd98940e55ce9707ef
Mariane Lacerda <113381114+MarianeLacerda@users.noreply.github.com>
2023-04-02 01:14:02 -0300

    Update Inicio.jsx

--- pages/Inicio/Inicio.jsx
+++ pages/Inicio/Inicio.jsx
@@ -2,13 +2,11 @@ import {Link} from "react-router-dom"
 import styles from "./Inicio.module.css"
 import img from "../../assets/img.png"
 import Navbar from "../../components/Navbar"
-import Main from "../../components/Main/Main"
 
 export function Inicio(){
     return(
 <>
 <Navbar/>
-<Main/>
 
 </>
     )
